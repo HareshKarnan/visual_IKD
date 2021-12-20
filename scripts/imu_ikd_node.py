d36e933d1459db454e72d6c2e9f1c84be99e9fff
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-20 12:07:58 -0600

    fixed history size at 5

--- scripts/imu_ikd_node.py
+++ scripts/imu_ikd_node.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     parser.add_argument('--input_topic', default='/ackermann_drive_init', type=str)
     parser.add_argument('--output_topic', default='/ackermann_curvature_drive',  type=str)
     parser.add_argument('--config_path', type=str, default="config/alphatruck.yaml")
-    parser.add_argument('--history_len', type=int, default=10)
+    parser.add_argument('--history_len', type=int, default=5)
     args = parser.parse_args()
 
     rospy.init_node('ikd_node', anonymous=True)
