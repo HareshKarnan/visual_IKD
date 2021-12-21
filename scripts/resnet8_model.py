b0d9ecfb0ec066e84cb29284715938b4e03869f7
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-21 17:38:40 -0600

    resnet8 model works now

--- scripts/resnet8_model.py
+++ scripts/resnet8_model.py
@@ -39,16 +39,16 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, embedding_size=16):
         super(ResNet, self).__init__()
-        self.in_planes = 128
-
-        self.conv1 = nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(128)
-        self.layer1 = self._make_layer(block,  128, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 256, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 512, num_blocks[2], stride=2)
+        self.in_planes = 8
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.layer1 = self._make_layer(block,  8, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 16, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 32, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 64, num_blocks[3], stride=2)
         self.dropout = nn.Dropout(p=0.5)
-        self.linear1 = nn.Linear(8192, 512) #512*exp*block.expansion
-        self.linear2 = nn.Linear(512, embedding_size) #512*exp*block.expansion
+        self.linear1 = nn.Linear(256, 128) #512*exp*block.expansion
+        self.linear2 = nn.Linear(128, embedding_size) #512*exp*block.expansion
         self.act = nn.PReLU()
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -64,6 +64,7 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
+        out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = nn.Flatten()(out)
         out = self.act(self.dropout(out))
@@ -72,4 +73,4 @@ class ResNet(nn.Module):
         return out
 
 def ResNet8(output_emb_size=16):
-    return ResNet(BasicBlock, [1, 1, 1], embedding_size=output_emb_size) #2048
+    return ResNet(BasicBlock, [1, 1, 1, 1], embedding_size=output_emb_size) #2048
