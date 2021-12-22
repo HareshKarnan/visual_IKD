bd19bf322e5493377c5db3e4f15d7e567f0f0329
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-21 18:00:56 -0600

    not use vision by default

--- scripts/arguments.py
+++ scripts/arguments.py
@@ -7,7 +7,7 @@ def get_args():
 	parser.add_argument('--batch_size', type=int, default=32)
 	parser.add_argument('--num_gpus', type=int, default=1)
 	parser.add_argument('--hidden_size', type=int, default=32)
-	parser.add_argument('--use_vision', action='store_true', default=True)
+	parser.add_argument('--use_vision', action='store_true', default=False)
 	parser.add_argument('--data_dir', type=str,
 						default='/home/haresh/PycharmProjects/visual_IKD/src/rosbag_sync_data_rerecorder/data/ahg_indoor_bags/')
 	parser.add_argument('--train_dataset_names', type=str, nargs='+', default=['train1_data',
