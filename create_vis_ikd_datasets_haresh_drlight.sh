b58f19810d6b76e8e6dc98840dd79f615f6cdcab
haresh92 <haresh.miriyala@gmail.com>
2021-12-25 16:18:19 -0600

    creating dataset script for dr-light machine

---
+++ create_vis_ikd_datasets_haresh_drlight.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run a loop from 1 to 10
+for i in {1..13}
+do
+  echo "processing rosbag train$i.bag"
+  rm /home/users/haresh92/visual_IKD/src/rosbag_sync_data_rerecorder/data/ahg_indoor_bags/train$i_data/*
+  roslaunch src/rosbag_sync_data_rerecorder/launch/rosbag_data_recorder.launch dataset:=train$i data_base_dir:=/home/users/haresh92/visual_IKD/src/rosbag_sync_data_rerecorder/data/ahg_indoor_bags out_base_dir:=/home/users/haresh92/visual_IKD/src/rosbag_sync_data_rerecorder/data/ahg_indoor_bags
+done
