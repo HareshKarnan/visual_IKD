84d2043d96110e7ccd2f7c4e7b2f2c9cd1b2e44c
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-26 14:58:14 -0600

    added waypoint recorder

--- scripts/waypoint_recorder.py
+++ scripts/waypoint_recorder.py
@@ -3,14 +3,29 @@
 import yaml
 import rospy
 from geometry_msgs.msg import PoseStamped
+from termcolor import cprint
 
 class WaypointRecorder:
 	def __init__(self):
-		self.waypoints = []
+		self.waypoints = {}
 		self.waypoint_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.waypoint_callback)
 
 	def waypoint_callback(self, msg):
-		self.waypoints.append(msg.pose)
+		cprint('Waypoint received', 'green', attrs=['bold'])
+		num_wpts = len(self.waypoints.keys())
+
+		self.waypoints[+1] = {}
+		self.waypoints[num_wpts+1]['xp'] = msg.pose.position.x
+		self.waypoints[num_wpts+1]['yp'] = msg.pose.position.y
+		self.waypoints[num_wpts+1]['zp'] = msg.pose.position.z
+		self.waypoints[num_wpts+1]['xr'] = msg.pose.orientation.x
+		self.waypoints[num_wpts+1]['yr'] = msg.pose.orientation.y
+		self.waypoints[num_wpts+1]['zr'] = msg.pose.orientation.z
+		self.waypoints[num_wpts+1]['wr'] = msg.pose.orientation.w
+
+		print('received waypoint : ', self.waypoints[num_wpts+1])
+
+		print(msg.pose)
 
 if __name__ == '__main__':
 	rospy.init_node('waypoint_recorder')
