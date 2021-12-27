4e563ae689db0d294c309e54e8ce6ac87e860ffb
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-27 16:20:18 -0600

    using localization msg to record waypoints

--- scripts/waypoint_recorder.py
+++ scripts/waypoint_recorder.py
@@ -4,22 +4,34 @@ import yaml
 import rospy
 from geometry_msgs.msg import PoseStamped
 from termcolor import cprint
+from amrl_msgs.msg import Localization2DMsg
+from scipy.spatial.transform import Rotation as R
 
 class WaypointRecorder:
 	def __init__(self):
 		self.waypoints = {}
-		self.waypoint_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.waypoint_callback)
+		# self.waypoint_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.waypoint_callback)
+		self.localization_sub = rospy.Subscriber('/localization', Localization2DMsg, self.localization_callback)
 
 	def waypoint_callback(self, msg):
 		cprint('Waypoint received', 'green', attrs=['bold'])
 		num_wpts = len(self.waypoints.keys())
 
 		self.waypoints[num_wpts+1] = {}
-		self.waypoints[num_wpts+1]['position'] = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+		self.waypoints[num_wpts+1]['position'] = [msg.pose.position.x, msg.pose.position.y]
 		self.waypoints[num_wpts+1]['orientation'] = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
 
 		print('received waypoint : ', self.waypoints[num_wpts+1])
 
+		print(msg.pose)
+
+	def localization_callback(self, msg):
+		cprint('Localization received', 'green', attrs=['bold'])
+		num_wpts = len(self.waypoints.keys())
+		self.waypoints[num_wpts+1] = {}
+		self.waypoints[num_wpts+1]['position'] = [msg.pose.x, msg.pose.y]
+		quaternion = R.from_euler('XYZ', [0, 0, msg.pose.theta], degrees=False).as_quat()
+		self.waypoints[num_wpts+1]['orientation'] = [quaternion[0], quaternion[1], quaternion[2], quaternion[3]]
 		print(msg.pose)
 
 if __name__ == '__main__':
