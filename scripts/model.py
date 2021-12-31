c6d3f33d278389d11e4d31a34999dcd13a1c5f1b
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-30 20:17:37 -0600

    using a smaller model

--- scripts/model.py
+++ scripts/model.py
@@ -10,21 +10,27 @@ class L2Normalize(nn.Module):
 class VisualIKDNet(nn.Module):
     def __init__(self, input_size, output_size, hidden_size=32):
         super(VisualIKDNet, self).__init__()
+        # self.visual_encoder = nn.Sequential(
+        #     nn.Conv2d(3, 6, kernel_size=5, stride=2), nn.ReLU(), # 30 x 30
+        #     nn.MaxPool2d(kernel_size=2, stride=2), # 15 x 15
+        #     nn.Conv2d(6, 12, kernel_size=3, stride=2), nn.ReLU(), # 7 x 7
+        #     nn.MaxPool2d(kernel_size=2, stride=2), # 3 x 3
+        #     nn.Flatten(),
+        #     nn.Linear(3*3*12, 16)
+        # )
+
         self.visual_encoder = nn.Sequential(
             nn.Conv2d(3, 6, kernel_size=5, stride=2), nn.ReLU(), # 30 x 30
-            nn.MaxPool2d(kernel_size=2, stride=2), # 15 x 15
-            nn.Conv2d(6, 12, kernel_size=3, stride=2), nn.ReLU(), # 7 x 7
-            nn.MaxPool2d(kernel_size=2, stride=2), # 3 x 3
+            nn.MaxPool2d(kernel_size=5, stride=2), # 13 x 13
             nn.Flatten(),
-            nn.Linear(3*3*12, 16)
+            nn.Linear(6*13*13, 16)
         )
 
         # self.visual_encoder = ResNet8(output_emb_size=16)
 
         self.imu_net = nn.Sequential(
             nn.Linear(200 * 3 + 60 * 3, 128), nn.BatchNorm1d(128), nn.PReLU(),
-            nn.Linear(128, 64), nn.BatchNorm1d(64), nn.PReLU(),
-            nn.Linear(64, 16),
+            nn.Linear(128, 16),
             nn.Dropout(p=0.1)
         )
 
@@ -36,8 +42,7 @@ class VisualIKDNet(nn.Module):
 
     def forward(self, accel, gyro, odom, image):
         visual_embedding = self.visual_encoder(image)
-        accel_gyro = torch.cat((accel, gyro), dim=1)
-        imu_embedding = self.imu_net(accel_gyro)
+        imu_embedding = self.imu_net(torch.cat((accel, gyro), dim=1))
         return self.ikdmodel(torch.cat((odom, imu_embedding, visual_embedding), dim=1))
 
 class SimpleIKDNet(nn.Module):
