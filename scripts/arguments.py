9551aee9677e71a4c1fe246f548d99ac0cec5fbf
Haresh Karnan <haresh.miriyala@gmail.com>
2022-02-16 10:45:50 -0600

    only using outdoor data

--- scripts/arguments.py
+++ scripts/arguments.py
@@ -11,17 +11,19 @@ def get_args():
 	parser.add_argument('--use_simple_vision', action='store_true', default=False)
 	parser.add_argument('--data_dir', type=str,
 						default='/home/haresh/PycharmProjects/visual_IKD/src/rosbag_sync_data_rerecorder/data/ahg_indoor_bags/')
-	parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data',
-																	 'train7_data', 'train9_data', 'train10_data', 'train11_data',
-																	 'train13_data', 'train14_data', 'train15_data',
-																	 'train17_data', 'train18_data', 'train20_data',
-																	 'train22_data', 'train24_data', 'train26_data'])
-	parser.add_argument('--val_dataset_names', nargs='+',
-						default=['train2_data', 'train4_data',
-								 'train6_data', 'train8_data',
-								 'train12_data', 'train16_data',
-								 'train23_data', 'train25_data',
-								 'train27_data',
-								 'train19_data', 'train21_data'])
+	# parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data',
+	# 																 'train7_data', 'train9_data', 'train10_data', 'train11_data',
+	# 																 'train13_data', 'train14_data', 'train15_data',
+	# 																 'train17_data', 'train18_data', 'train20_data',
+	# 																 'train22_data', 'train24_data', 'train26_data'])
+	# parser.add_argument('--val_dataset_names', nargs='+',
+	# 					default=['train2_data', 'train4_data',
+	# 							 'train6_data', 'train8_data',
+	# 							 'train12_data', 'train16_data',
+	# 							 'train23_data', 'train25_data',
+	# 							 'train27_data',
+	# 							 'train19_data', 'train21_data'])
+	parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data','train6_data'])
+	parser.add_argument('--val_dataset_names', nargs='+', default=['train2_data', 'train4_data'])
 
 	return parser.parse_args()
