5b6ad9145aed53394565779653af0d77de0e2895
Haresh Karnan <haresh.miriyala@gmail.com>
2022-02-17 10:43:43 -0600

    using batchnorm

--- scripts/model.py
+++ scripts/model.py
@@ -23,28 +23,24 @@ class VisualIKDNet(nn.Module):
         # )
 
         self.visual_encoder = nn.Sequential(
-             nn.Conv2d(3, 16, kernel_size=3, stride=2), nn.PReLU(), # 31x31
+             nn.Conv2d(3, 16, kernel_size=3, stride=2), nn.BatchNorm2d(16), nn.PReLU(), # 31x31
              nn.MaxPool2d(kernel_size=3, stride=2), # 15x15
-             nn.Conv2d(16, 32, kernel_size=3, stride=2), nn.PReLU(), # 7x7
+             nn.Conv2d(16, 32, kernel_size=3, stride=2), nn.BatchNorm2d(32), nn.PReLU(), # 7x7
              nn.MaxPool2d(kernel_size=3, stride=2), # 3x3
-             nn.Flatten(), nn.Dropout(0.1),
-             nn.Linear(3*3*32, 64), nn.PReLU(),
+             nn.Flatten(),
+             nn.Linear(3*3*32, 64), nn.BatchNorm1d(64), nn.PReLU(),
              nn.Linear(64, 16)
         )
         
         self.imu_net = nn.Sequential(
-            nn.Linear(200 * 3 + 60 * 3, 128), nn.PReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(128, 64), nn.PReLU(),
-            nn.Dropout(0.1),
+            nn.Linear(200 * 3 + 60 * 3, 128), nn.BatchNorm1d(128), nn.PReLU(),
+            nn.Linear(128, 64), nn.BatchNorm1d(64), nn.PReLU(),
             nn.Linear(64, 16),
         )
 
         self.ikdmodel = nn.Sequential(
-            nn.Linear(2 + 16 + 16, hidden_size), nn.PReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size), nn.PReLU(),
-            nn.Dropout(0.1),
+            nn.Linear(2 + 16 + 16, hidden_size), nn.BatchNorm1d(hidden_size), nn.PReLU(),
+            nn.Linear(hidden_size, hidden_size), nn.BatchNorm1d(hidden_size), nn.PReLU(),
             nn.Linear(hidden_size, hidden_size), nn.PReLU(),
             nn.Linear(hidden_size, output_size),
         )
@@ -65,20 +61,16 @@ class SimpleIKDNet(nn.Module):
     def __init__(self, input_size, output_size, hidden_size=32):
         super(SimpleIKDNet, self).__init__()
         self.imu_net = nn.Sequential(
-            nn.Linear(200*3 + 60 * 3, 128), nn.PReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(128, 64), nn.PReLU(),
-            nn.Dropout(0.1),
+            nn.Linear(200*3 + 60 * 3, 128), nn.BatchNorm1d(128), nn.PReLU(),
+            nn.Linear(128, 64), nn.BatchNorm1d(64), nn.PReLU(),
             nn.Linear(64, 16),
         )
 
         self.skip = nn.Linear(200*3 + 60 * 3, output_size)
 
         self.ikdmodel = nn.Sequential(
-            nn.Linear(2 + 16, hidden_size), nn.PReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size), nn.PReLU(),
-            nn.Dropout(0.1),
+            nn.Linear(2 + 16, hidden_size), nn.BatchNorm1d(128), nn.PReLU(),
+            nn.Linear(hidden_size, hidden_size), nn.BatchNorm1d(hidden_size), nn.PReLU(),
             nn.Linear(hidden_size, hidden_size), nn.PReLU(),
             nn.Linear(hidden_size, output_size),
         )
