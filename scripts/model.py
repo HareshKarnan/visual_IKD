0d1f100e85afdec3495ff5f5e487ca3013db438d
Haresh Karnan <haresh.miriyala@gmail.com>
2022-02-17 21:27:57 -0600

    reverting back to using big net

--- scripts/model.py
+++ scripts/model.py
@@ -27,27 +27,28 @@ class VisualIKDNet(nn.Module):
              nn.MaxPool2d(kernel_size=3, stride=2), # 15x15
              nn.Conv2d(16, 32, kernel_size=3, stride=2, bias=False), nn.PReLU(), # 7x7
              nn.MaxPool2d(kernel_size=3, stride=2), # 3x3
-             nn.Flatten(),
-             nn.Linear(3*3*32, 64), nn.PReLU(),
-             nn.Linear(64, 16)
+             nn.Flatten(), nn.Dropout(0.1),
+             nn.Linear(3*3*32, 16)
         )
         
         self.imu_net = nn.Sequential(
-            nn.Linear(200 * 3 + 60 * 3, 256), nn.PReLU(),
-            nn.Linear(256, 4), nn.PReLU(),
-            nn.Linear(4, 2),
+            nn.Linear(200 * 3 + 60 * 3, 256), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 256), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 16),
         )
-        self.imu_net_skip = nn.Linear(200 * 3 + 60 * 3, 2, bias=False)
+        self.imu_net_skip = nn.Linear(200 * 3 + 60 * 3, 16, bias=False)
 
         self.ikdmodel = nn.Sequential(
-            nn.Linear(2 + 2 + 16, hidden_size), nn.PReLU(),
+            nn.Linear(2 + 16 + 16, hidden_size), nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size), nn.PReLU(),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(hidden_size, hidden_size), nn.PReLU(),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
             nn.Linear(hidden_size, output_size),
         )
-        self.ikdmodel_skip = nn.Linear(2 + 2 + 16, output_size, bias=False)
+        self.ikdmodel_skip = nn.Linear(2 + 16 + 16, output_size, bias=False)
 
         self.full_skip = nn.Linear(200*3 + 60 * 3, output_size, bias=False)
 
@@ -64,20 +65,25 @@ class SimpleIKDNet(nn.Module):
     def __init__(self, input_size, output_size, hidden_size=32):
         super(SimpleIKDNet, self).__init__()
         self.imu_net = nn.Sequential(
-            nn.Linear(200*3 + 60 * 3, 256), nn.PReLU(),
-            nn.Linear(256, 4), nn.PReLU(),
-            nn.Linear(4, 2),
+            nn.Linear(200*3 + 60 * 3, 256), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 256), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 16),
         )
-        self.imu_net_skip = nn.Linear(200*3 + 60 * 3, 2, bias=False)
+        self.imu_net_skip = nn.Linear(200*3 + 60 * 3, 16, bias=False)
 
         self.full_skip = nn.Linear(200*3 + 60 * 3, output_size, bias=False)
 
         self.ikdmodel = nn.Sequential(
-            nn.Linear(2 + 2, hidden_size), nn.PReLU(),
-            nn.Linear(hidden_size, hidden_size), nn.PReLU(),
+            nn.Linear(2 + 16, hidden_size), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
             nn.Linear(hidden_size, output_size),
         )
-        self.ikdmodel_skip = nn.Linear(2 + 2, output_size, bias=False)
+        self.ikdmodel_skip = nn.Linear(2 + 16, output_size, bias=False)
 
     def forward(self, accel, gyro, odom):
         accel_gyro = torch.cat((accel, gyro), dim=1)
