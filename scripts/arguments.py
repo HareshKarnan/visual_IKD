4384996916363c799d856fd19906594ecb5f60e9
Haresh Karnan <haresh.miriyala@gmail.com>
2022-02-17 21:17:48 -0600

    using exact model description as xuesu

--- scripts/arguments.py
+++ scripts/arguments.py
@@ -13,20 +13,20 @@ def get_args():
 	parser.add_argument('--use_simple_vision', action='store_true', default=False)
 	parser.add_argument('--data_dir', type=str,
 						default='/home/haresh/PycharmProjects/visual_IKD/src/rosbag_sync_data_rerecorder/data/outdoor_bags/')
-	parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data',
-																	 'train7_data', 'train9_data', 'train10_data', 'train11_data',
-																	 'train13_data', 'train14_data', 'train15_data',
-																	 'train17_data', 'train18_data', 'train20_data',
-																	 'train22_data'])
-	parser.add_argument('--val_dataset_names', nargs='+',
-						default=['train2_data', 'train4_data',
-								 'train6_data', 'train8_data',
-								 'train12_data', 'train16_data',
-								 'train21_data', 'train19_data'])
-	parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data','train7_data', 'train9_data',
-																	 'train11_data', 'train12_data','train14_data', 'train16_data',
-																	 'train18_data', 'train20_data', 'train21_data', 'train22_data'])
-	parser.add_argument('--val_dataset_names', nargs='+', default=['train2_data', 'train4_data', 'train6_data', 'train8_data' 'train10_data',
-																   'train13_data', 'train15_data', 'train17_data', 'train19_data'])
+	# parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data',
+	# 																 'train7_data', 'train9_data', 'train10_data', 'train11_data',
+	# 																 'train13_data', 'train14_data', 'train15_data',
+	# 																 'train17_data', 'train18_data', 'train20_data',
+	# 																 'train22_data'])
+	# parser.add_argument('--val_dataset_names', nargs='+',
+	# 					default=['train2_data', 'train4_data',
+	# 							 'train6_data', 'train8_data',
+	# 							 'train12_data', 'train16_data',
+	# 							 'train21_data', 'train19_data'])
+	# parser.add_argument('--train_dataset_names', nargs='+', default=['train1_data', 'train3_data', 'train5_data','train7_data', 'train9_data',
+	# 																 'train11_data', 'train12_data','train14_data', 'train16_data',
+	# 																 'train18_data', 'train20_data', 'train21_data', 'train22_data'])
+	# parser.add_argument('--val_dataset_names', nargs='+', default=['train2_data', 'train4_data', 'train6_data', 'train8_data' 'train10_data',
+	# 															   'train13_data', 'train15_data', 'train17_data', 'train19_data'])
 
 	return parser.parse_args()
